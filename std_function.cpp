526ac33f37067f91b97c3e46c92a0bf3374216d0
oda3174964 <yohasakura@126.com>
2016-10-19 22:32:28 +0800

    std::function

---
+++ std_function.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <functional>
+
+class A
+{
+	std::function<void()> callback_;
+
+public:
+	A(const std::function<void()>& f) :callback_(f)
+	{}
+
+	void notify(void)
+	{
+		callback_();
+	}
+};
+
+class Foo
+{
+public:
+	void operator()(void)
+	{
+		std::cout << __FUNCTION__ << std::endl;
+	}
+};
+
+int main()
+{
+	Foo foo;
+	A aa(foo);
+	aa.notify();
+
+	return 0;
+}
